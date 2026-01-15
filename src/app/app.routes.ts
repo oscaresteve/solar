c2630463ceaca1ce6ad50a8bc18e15ecfebb40d9
oscaresteve <oscarestevea24@gmail.com>
2026-01-15 09:32:48 +0100

    Router Outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PlantaList } from './plantas/planta-list/planta-list';
 import { PlantaDetail } from './plantas/planta-detail/planta-detail';
 import { Home } from './components/home/home';
+import { PlantaTable } from './plantas/planta-table/planta-table';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'plantas', component: PlantaList },
+  { path: 'tabla', component: PlantaTable },
   { path: 'planta/:id', component: PlantaDetail },
   { path: '**', pathMatch: 'full', redirectTo: '#home' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
-import { PlantaTable } from "./plantas/planta-table/planta-table";
+import { PlantaTable } from './plantas/planta-table/planta-table';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, PlantaTable],
+  imports: [Header, PlantaTable, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
