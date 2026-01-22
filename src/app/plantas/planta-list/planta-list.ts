1a95e84ec652461c184d11bcbcd6d884a1feefce
oscaresteve <oscarestevea24@gmail.com>
2026-01-22 08:50:07 +0100

    Unsubscribe al final del ciclo de vida del componente

--- src/app/plantas/planta-list/planta-list.ts
+++ src/app/plantas/planta-list/planta-list.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PlantaItem } from '../planta-item/planta-item';
 import { Planta } from '../planta';
 import { SupabaseService } from '../../services/supabase-service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-planta-list',
@@ -9,18 +10,23 @@ import { SupabaseService } from '../../services/supabase-service';
   templateUrl: './planta-list.html',
   styleUrl: './planta-list.css',
 })
-export class PlantaList implements OnInit {
+export class PlantaList implements OnInit, OnDestroy {
   private supabaseService: SupabaseService = inject(SupabaseService);
 
   plantas = signal<Planta[]>([]);
+  plantasSubscription?: Subscription;
 
   ngOnInit(): void {
-    this.supabaseService
+    this.plantasSubscription = this.supabaseService
       .readPlantas()
       .subscribe((plantasSupabase: Planta[]) => this.plantas.set(plantasSupabase));
   }
 
   toggleFavorite(planta: Planta) {
     planta.favorite = !planta.favorite;
+  }
+
+  ngOnDestroy(): void {
+    this.plantasSubscription && this.plantasSubscription.unsubscribe();
   }
 }
