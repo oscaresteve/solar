e0a7bd700c389808cd03716c65ddd2c0592c3068
oscaresteve <oscarestevea24@gmail.com>
2026-01-30 13:19:32 +0100

    Estilos

--- src/app/plantas/features/planta-detail/planta-detail.ts
+++ src/app/plantas/features/planta-detail/planta-detail.ts
@@ -2,9 +2,10 @@ import { Component, computed, inject, Input, OnInit } from '@angular/core';
 import { PlantaService } from '../../data-access/planta-service';
 import { PlantaLogsList } from '../../../planta-logs/features/planta-logs-list/planta-logs-list';
 import { PlantaLogsService } from '../../../planta-logs/data-access/planta-logs-service';
+import { Navbar } from '../../../shared/features/navbar/navbar';
 @Component({
   selector: 'app-planta-detail',
-  imports: [PlantaLogsList],
+  imports: [PlantaLogsList, Navbar],
   templateUrl: './planta-detail.html',
   styleUrl: './planta-detail.scss',
 })
