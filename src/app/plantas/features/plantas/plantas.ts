8b73234f88bc95414914e1f328f6cf14c2ef7e3f
oscaresteve <oscarestevea24@gmail.com>
2026-02-16 13:42:45 +0100

    Boton Crear Planta

--- src/app/plantas/features/plantas/plantas.ts
+++ src/app/plantas/features/plantas/plantas.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { PlantaTable } from '../planta-table/planta-table';
 import { Navbar } from '../../../shared/features/navbar/navbar';
 import { PlantaService } from '../../data-access/planta-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-plantas',
-  imports: [PlantaTable, Navbar],
+  imports: [PlantaTable, Navbar, RouterLink],
   templateUrl: './plantas.html',
   styleUrl: './plantas.scss',
 })
