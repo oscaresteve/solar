aa7032ce2a45c515284df3c8e6a6af04d89dd78c
oscaresteve <oscarestevea24@gmail.com>
2026-02-23 18:15:58 +0100

    Navbar y footer en App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Footer } from './shared/features/footer/footer';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/features/navbar/navbar';
+import { Footer } from './shared/features/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
+  private readonly router = inject(Router);
+
   protected readonly title = signal('solar');
+
+  protected isAuthRoute(): boolean {
+    return this.router.url.startsWith('/auth');
+  }
 }
