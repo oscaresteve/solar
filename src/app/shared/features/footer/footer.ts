426462aaf30feb40fdfc14db083e53c709ba89fb
oscaresteve <oscarestevea24@gmail.com>
2026-02-23 13:46:50 +0100

    Mejoras en diseño

--- src/app/shared/features/footer/footer.ts
+++ src/app/shared/features/footer/footer.ts
@@ -1,11 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
-
+  today = new Date();
 }
