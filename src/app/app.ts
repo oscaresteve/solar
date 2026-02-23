1042d05b6beaa4bc5c50adf0818ee3c00356918e
oscaresteve <oscarestevea24@gmail.com>
2026-02-23 12:52:27 +0100

    Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './shared/features/footer/footer';
+import { Navbar } from './shared/features/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
