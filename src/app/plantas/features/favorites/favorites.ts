0c23b04dd3750da91f45cc3b6a2c5ff0bba89ed2
oscaresteve <oscarestevea24@gmail.com>
2026-02-24 14:05:13 +0100

    Mensajes de Error y Empty en Favoritos

--- src/app/plantas/features/favorites/favorites.ts
+++ src/app/plantas/features/favorites/favorites.ts
@@ -2,10 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FavoritesService } from '../../data-access/favorites-service';
 import { Planta } from '../../interfaces/planta';
 import { PlantaItem } from '../planta-item/planta-item';
+import { CardSkeleton } from '../../../shared/ui/card-skeleton/card-skeleton';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favoritos',
-  imports: [PlantaItem],
+  imports: [PlantaItem, CardSkeleton, RouterLink],
   templateUrl: './favorites.html',
   styleUrl: './favorites.scss',
 })
@@ -15,6 +17,10 @@ export class Favorites implements OnInit {
   plantasFavorites = this._favoritesService.plantasFavorites;
   loading = this._favoritesService.loading;
   error = this._favoritesService.error;
+
+  onRetryLoad() {
+    this._favoritesService.readFavorites();
+  }
 
   isFavorite(plantaId: string) {
     return this._favoritesService.isFavorite(plantaId);
