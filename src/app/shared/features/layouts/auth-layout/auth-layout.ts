d1b3264c72bc68250c464895744b6ee505659597
oscaresteve <oscarestevea24@gmail.com>
2026-02-27 16:39:23 +0100

    Estilos

--- src/app/shared/features/layouts/auth-layout/auth-layout.ts
+++ src/app/shared/features/layouts/auth-layout/auth-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from '../../footer/footer';
 
 @Component({
   selector: 'auth-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './auth-layout.html',
 })
 export class AuthLayout {}
