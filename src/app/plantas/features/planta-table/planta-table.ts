e4c75f4ec899a1c650bf1791d927d3182ca7c8b5
oscaresteve <oscarestevea24@gmail.com>
2026-03-05 19:03:01 +0100

    Estilos en Planta Table y Planta Table Row

--- src/app/plantas/features/planta-table/planta-table.ts
+++ src/app/plantas/features/planta-table/planta-table.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PlantaTableRow } from '../planta-table-row/planta-table-row';
 import { TableRowSkeleton } from '../../../shared/ui/table-row-skeleton/table-row-skeleton';
 import { RouterLink } from '@angular/router';
+import { Icon } from '../../../shared/ui/icon/icon';
 
 @Component({
   selector: 'app-planta-table',
-  imports: [CommonModule, PlantaTableRow, TableRowSkeleton, RouterLink],
+  imports: [CommonModule, PlantaTableRow, TableRowSkeleton, RouterLink, Icon],
   templateUrl: './planta-table.html',
   styleUrl: './planta-table.scss',
 })
